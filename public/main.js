60ba1504d4f602dcbee9b0f965501e637ecb9a3a
Gustavo Tomás <guga.tomas10@gmail.com>
2021-10-20 18:20:30 -0300

    added invoke to submit form listener

--- public/main.js
+++ public/main.js
@@ -1,11 +1,20 @@
 const form = document.querySelector('form');
-const actionInput = document.querySelector('#action');
+const [action, task] = document.getElementById('main-form').elements;
+// const actionInput = document.querySelector('#action');
 
+/**
+ * Sends Action and Activity to Rust back-end by calling function
+ * handle_action and sending object with fields values as parameters
+ */
 form.addEventListener('submit', evt => {
   evt.preventDefault();
-  console.log(evt.target);
+  console.log("action: ", action.value, "\ntask: ", task.value);
+  __TAURI__.invoke('handle_action', {
+    action: action.value,
+    task: task.value
+  });
 });
 
-actionInput.addEventListener('input', evt => {
-  console.log("Value: ", actionInput.value);
-});
+// actionInput.addEventListener('input', evt => {
+//   console.log("Value: ", actionInput.value);
+// });
