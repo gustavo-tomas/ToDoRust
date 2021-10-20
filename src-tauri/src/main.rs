7bba05eddd60e536dd100a3fcfa302bf00abbecd
Gustavo Tomás <guga.tomas10@gmail.com>
2021-10-20 18:17:28 -0300

    added command for handling forms

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,20 @@
   windows_subsystem = "windows"
 )]
 
+use tauri::{command};
+
+#[command]
+fn handle_action(action: String, task: String) {
+  println!("ACTION: {}, TASK: {}", action, task);
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(
+      tauri::generate_handler![
+        handle_action,
+      ]
+    )
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
