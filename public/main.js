5c2aa8b0ac653e57cf72cc3884f85c4ceed1ada7
Gustavo Tomás <guga.tomas10@gmail.com>
2021-10-21 10:23:31 -0300

    added alerts and dynamic updates to the list

--- public/main.js
+++ public/main.js
@@ -4,37 +4,93 @@ const taskList = document.querySelector('#task-list');
 // const actionInput = document.querySelector('#action');
 
 /**
- * Sends Action and Activity to Rust back-end by calling function
- * handle_action and sending object with fields values as parameters
+ * Render all the tasks retrieved from the database as items in a list.
+ * Cleans the current screen before updating.
  */
-form.addEventListener('submit', evt => {
-  evt.preventDefault();
-  console.log("action: ", action.value, "\ntask: ", task.value);
-  __TAURI__.invoke('update_todo', {
-    action: action.value.toLowerCase(),
-    task: task.value.toLowerCase()
-  });
-});
-
-/**
- * Renders tasks on load
- * @TODO finish
- */
-window.addEventListener('load', () => {
-  console.log("WERE IN")
-  
+const renderTasks = () => {
+  taskList.innerHTML = "";
   __TAURI__.invoke("get_task").then(result => {
-    console.log("RESULT LENGTH: ", result.length);
-    console.log("RESULT: ", [...result]);
-    const task = document.createElement('li');
-    task.setAttribute('class', 'task-item');
     result.forEach(element => {
+      const task = document.createElement('li');
+      task.setAttribute('class', 'task-item');
       task.textContent = element;
       taskList.appendChild(task);
     });
   }).catch(() => {
-    console.log("Rejected promise to render tasks");
+    console.error("Rejected promise to render tasks");
   });
+}
+
+/**
+ * Render a task as an item in the list
+ * @TODO make it prettier
+ * @param {result of the submitted form} result 
+ */
+const renderTask = (result) => {
+  let flag = false;
+  taskList.childNodes.forEach(child => {
+    if (child.innerText == result.task) {
+      flag = true;
+    }
+  });
+
+  if (!flag) {
+    const task = document.createElement('li');
+    task.setAttribute('class', 'task-item');
+    task.textContent = result.task;
+    taskList.appendChild(task);
+  }
+  else {
+    window.alert(result.task + " is already in the list!");
+  }
+}
+
+/**
+ * Removes selected task if present in the list
+ * @TODO make it prettier
+ * @param {result is the object {action: task: } to be removed} result 
+ */
+const removeRenderedTask = (result) => {
+  let flag = false;
+  taskList.childNodes.forEach(child => {
+    if (child.innerText == result.task) {
+      taskList.removeChild(child);
+      flag = true;
+    }
+  });
+  if (!flag) {
+    window.alert(result.task + " is not in the list!");
+  }
+}
+
+// Sends Action and Task to Rust back-end by calling function
+// handle_action and sending object with fields values as parameters
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  console.log("action: ", action.value, "\ntask: ", task.value);
+  const result = {
+    action: action.value.toLowerCase(),
+    task: task.value.toLowerCase()
+  };
+  __TAURI__.invoke('update_todo', result).then(() => {
+    if (result.action == 'add') {
+      renderTask(result);
+    }
+    else if (result.action == 'remove') {
+      removeRenderedTask(result);
+    }
+    else {
+      window.alert(result.action + " is not a valid action!");
+    }
+  }).catch(() => {
+    console.error("Rejected promise to update list");
+  });
+});
+
+// Render tasks on load
+window.addEventListener('load', evt => {
+  evt.preventDefault();
+  renderTasks();
 });
 
 // actionInput.addEventListener('input', evt => {
